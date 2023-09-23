cb18b5cdb41d2eec0242d5e2deb4e29992a70506
kashishmamania777 <kashishmamania7@gmmail.com>
2023-09-23 10:54:16 +0530

    Done

--- moodtracker.py
+++ moodtracker.py
@@ -1,6 +1,6 @@
+from tkinter import Tk, Label, Button, StringVar
+from tkinter.ttk import Combobox
 from PIL import Image, ImageTk
-import tkinter as tk
-from tkinter import ttk
 
 # Initialize a dictionary to store mood data
 mood_data = {}
@@ -8,69 +8,68 @@ mood_data = {}
 # Define a list of days of the week
 days_of_week = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
 
-# Initialize variables to track the maximum mood
-max_mood = None
-max_day = None
-
-# Function to update mood data for the current day
+# Create a function to update mood data
 def update_mood():
-    day = days_of_week[len(mood_data)]  # Get the day based on the number of mood entries
     mood = mood_var.get()
+    if mood:
+        mood_data[current_day] = mood
+        next_day()
 
-    if mood in [1, 2, 3]:
-        mood_data[day] = mood
-        mood_var.set(0)  # Clear the mood input field
-        update_image(mood)
-        check_max_mood()
+# Create a function to switch to the next day
+def next_day():
+    global current_day
+    if not current_day:
+        current_day = days_of_week[0]
+    else:
+        current_day_index = days_of_week.index(current_day)
+        if current_day_index < len(days_of_week) - 1:
+            current_day = days_of_week[current_day_index + 1]
+        else:
+            current_day = None
+            show_summary()
 
-# Function to update the mood image
-def update_image(mood):
-    mood_images = {
-        1: "unknown.png",
-        2: "unknown.png",
-        3: "unknown.png"
-    }
+    day_label.config(text=f"Select Mood for {current_day}:")
+    mood_var.set("")
 
-    mood_image_path = mood_images.get(mood, "unknown.png")
-    mood_image = Image.open(mood_image_path)
-    #mood_image = mood_image.resize((100, 100), Image.ANTIALIAS)
-    mood_photo = ImageTk.PhotoImage(mood_image)
-    mood_label.config(image=mood_photo)
-    mood_label.image = mood_photo
+# Create a function to display the mood summary
+def show_summary():
+    summary_window = Tk()
+    summary_window.title("Mood Summary")
 
-# Function to check and display the maximum mood
-def check_max_mood():
-    global max_mood, max_day
-    if not mood_data:
-        return
-    max_mood = max(mood_data.values())
-    max_day = [day for day, mood in mood_data.items() if mood == max_mood][0]
-    max_mood_label.config(text=f"Max Mood: {max_mood} ({max_day})")
+    # Create a Label for each day's mood
+    for day, mood in mood_data.items():
+        mood_label = Label(summary_window, text=f"{day}: {mood}")
+        mood_label.pack()
+
+    # Create a Label for the max mood of the week
+    max_mood = max(mood_data.values(), key=lambda x: int(x))
+    max_mood_label = Label(summary_window, text=f"Max Mood of the Week: {max_mood}")
+    max_mood_label.pack()
+
+    summary_window.mainloop()
 
 # Create the main window
-root = tk.Tk()
+root = Tk()
 root.title('Mood Tracker')
 
 # Create GUI elements
-mood_label = tk.Label(root, text='Select Mood:')
-mood_var = tk.IntVar()
-mood_combobox = ttk.Combobox(root, textvariable=mood_var, values=[1, 2, 3])
-update_button = tk.Button(root, text='Update Mood', command=update_mood)
+current_day = days_of_week[0]
 
-mood_image = Image.open("unknown.png")  # Default image
-#mood_image = mood_image.resize((100, 100), Image.ANTIALIAS)
-mood_photo = ImageTk.PhotoImage(mood_image)
-mood_label = tk.Label(root, image=mood_photo)
-mood_label.image = mood_photo
+day_label = Label(root, text=f"Select Mood for {current_day}:")
+mood_var = StringVar()
+mood_combobox = Combobox(root, textvariable=mood_var, values=["1 - Happy", "2 - Okay", "3 - Sad"])
+update_button = Button(root, text='Update Mood', command=update_mood)
 
-max_mood_label = tk.Label(root, text="Max Mood: N/A")
+# Load the default "unknown.png" image for mood selection
+default_mood_image = Image.open("unknown.png")
+default_mood_image = default_mood_image.resize((200, 200), Image.ANTIALIAS)
+default_mood_image = ImageTk.PhotoImage(default_mood_image)
+mood_image_label = Label(root, image=default_mood_image)
 
 # Grid layout
-mood_label.grid(row=0, column=0, padx=10, pady=10)
-mood_combobox.grid(row=0, column=1, padx=10, pady=10)
-update_button.grid(row=1, column=0, columnspan=2, padx=10, pady=10)
-mood_label.grid(row=2, column=0, columnspan=2, padx=10, pady=10)
-max_mood_label.grid(row=3, column=0, columnspan=2, padx=10, pady=10)
+day_label.grid(row=0, column=0, padx=10, pady=10, columnspan=2)
+mood_combobox.grid(row=1, column=0, padx=10, pady=10, columnspan=2)
+update_button.grid(row=2, column=0, columnspan=2, padx=10, pady=10)
+mood_image_label.grid(row=3, column=0, columnspan=2, padx=10, pady=10)
 
-# Start the GUI main loop
 root.mainloop()
